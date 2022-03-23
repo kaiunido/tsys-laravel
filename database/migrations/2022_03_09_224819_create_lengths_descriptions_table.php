00e79abb8ec513d180c891d8f2ba2ca27d48a724
Carlos <kaiunido@gmail.com>
2022-03-23 20:25:04 -0300

    criar migration e seeds da tabela lengths

---
+++ database/migrations/2022_03_09_224819_create_lengths_descriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('lengths_descriptions', function (Blueprint $table) {
+      $table->foreignId('length_id')->constrained();
+      $table->foreignId('language_id')->constrained();
+      $table->string('name');
+      $table->string('unit', 3);
+      $table->timestamps();
+
+      $table->unique(['length_id', 'language_id']);
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('lengths_descriptions');
+  }
+};
