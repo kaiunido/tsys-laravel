308d9d5119fa89fb1d33ab25a641fc5d222361df
Carlos <kaiunido@gmail.com>
2022-03-23 01:24:26 -0300

    criar controller de autenticação

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use App\Http\Requests\AuthRequest;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+  /**
+   * Handle an authentication attempt
+   * 
+   * @param \Illuminate\Http\Request $request
+   * @return \Illuminate\Http\Response
+   */
+  public function login(AuthRequest $request)
+  {
+    $credential = $request->safe()->all();
+
+    if (!Auth::attempt($credential)) {
+      return response([
+        "status" => 401,
+        "errors" => ["auth" => __("auth.failed")],
+      ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    $request->session()->regenerate();
+
+    $user = User::select("id", "name", "email")
+      ->where("email", $credential["email"])->first();
+
+    if ($user->tokens) {
+      $user->tokens()->delete();
+      unset($user["tokens"]);
+    }
+
+    $token = $user->createToken("user_session")->plainTextToken;
+
+    return response([
+      "user" => $user,
+      "token" => $token,
+    ], Response::HTTP_OK);
+  }
+}
