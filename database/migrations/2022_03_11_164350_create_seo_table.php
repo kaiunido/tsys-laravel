d940a8a29d0e50f7b33fe76dd05a2ad4895792ec
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-02 04:50:11 -0300

    alterar campos nullables

--- database/migrations/2022_03_11_164350_create_seo_table.php
+++ database/migrations/2022_03_11_164350_create_seo_table.php
@@ -19,10 +19,10 @@ return new class extends Migration
       $table->foreignId('searchable_id');
       $table->foreignId('language_id');
       $table->string('meta_title');
-      $table->string('meta_description');
-      $table->string('meta_tags');
-      $table->string('query');
-      $table->string('meta_url');
+      $table->string('meta_description')->nullable();
+      $table->string('meta_tags')->nullable();
+      $table->string('query')->nullable();
+      $table->string('meta_url')->nullable();
       $table->timestamps();
       $table->softDeletes();
     });
