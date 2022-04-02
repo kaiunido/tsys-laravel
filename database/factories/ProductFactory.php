53f2d0993040b3c255665507ea4c7283612afba9
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-02 04:49:50 -0300

    ajustar o factory de produtos para o novo padrão

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -18,14 +18,15 @@ class ProductFactory extends Factory
   {
     return [
       'model' => $this->faker->shuffle($this->faker->regexify('[A-Z]{5}[0-9]{4}')),
-      'isbn' => $this->faker->isbn13(),
+      'isbn13' => $this->faker->isbn13(),
+      'condition' => $this->faker->numberBetween(0, 1),
       'location' => $this->faker->regexify('[A-Z]{3}[0-9]{3}'),
-      'quantity' => $this->faker->randomNumber(2),
+      //'quantity' => $this->faker->randomNumber(2),
       'stock_status_id' => $this->faker->numberBetween(1, 3),
       'image' => $this->faker->imageUrl(800, 600, 'Products', true),
       'manufacturer_id' => 1,
       'shipping' => $this->faker->numberBetween(0, 1),
-      'price' => $this->faker->randomFloat(2, 0, 500),
+      //'price' => $this->faker->randomFloat(2, 0, 500),
       'date_available' => $this->faker->dateTimeBetween('-5 years', '+1 month'),
       'weight' => $this->faker->randomFloat(2, 0.1, 2),
       'weight_id' => $this->faker->numberBetween(1, 2),
