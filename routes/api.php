699bfd3e66a79912d02b289e9d02a3f0a71455ba
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-05 09:10:11 -0300

    alterar para enviar apenas o nome e e-mail do usuário

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/usuario', function (Request $request) {
-  return $request->user();
+  return $request->user()->only(['name', 'email']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
