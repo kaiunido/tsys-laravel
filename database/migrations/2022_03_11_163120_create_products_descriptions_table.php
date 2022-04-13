1c990cdb5e3384096b31ee912a88cbf464882e95
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-13 02:42:02 -0300

    adicionar surrogate key para o eloquent

--- database/migrations/2022_03_11_163120_create_products_descriptions_table.php
+++ database/migrations/2022_03_11_163120_create_products_descriptions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
   public function up()
   {
     Schema::create('product_descriptions', function (Blueprint $table) {
+      $table->id();
       $table->foreignId('product_id');
       $table->foreignId('language_id');
       $table->string('name');
