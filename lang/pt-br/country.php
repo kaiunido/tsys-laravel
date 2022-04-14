a7100af66f74ede6a9274ed187b62e45b61cdcad
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-14 12:31:44 -0300

    criar arquivo de i18n para países

---
+++ lang/pt-br/country.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Country Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during country methods for various
+    | messages that we need to display to the country. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'deleted' => 'O país de ID :id foi removido.',
+    'not_found' => 'O país de ID :id não foi encontrado.'
+
+
+];
