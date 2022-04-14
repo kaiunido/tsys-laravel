299ba126eefe3e5dafb13c0e05c1b062ecc1e925
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-14 16:57:29 -0300

    separar regras de validação para store e update

--- app/Http/Requests/CountryRequest.php
+++ app/Http/Requests/CountryRequest.php
@@ -7,6 +7,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class CountryRequest extends FormRequest
 {
     /**
+     * Model utilizada nas regras
+     * 
+     * @var string
+     */
+    private $table = 'App\Models\Country';
+
+    /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
@@ -23,10 +30,36 @@ class CountryRequest extends FormRequest
      */
     public function rules(): array
     {
+        return $this->method() === 'PATCH' ? $this->updateRules() : $this->storeRules();
+    }
+
+    /**
+     * Regras de validação para o Store
+     * 
+     * @return array
+     */
+    private function storeRules(): array
+    {
         return [
-            'country.name' => 'required|string|unique:countries,name',
-            'country.alfa2' => 'required|string|size:2|unique:countries,alfa2',
-            'country.alfa3' => 'required|string|size:3|unique:countries,alfa3'
+            'country.name' => "required|string|unique:{$this->table},name,NULL,NULL,deleted_at,NULL",
+            'country.alfa2' => "required|string|size:2|unique:{$this->table},alfa2,NULL,NULL,deleted_at,NULL",
+            'country.alfa3' => "required|string|size:3|unique:{$this->table},alfa3,NULL,NULL,deleted_at,NULL"
+        ];
+    }
+
+    /**
+     * Regras de validação para o Update/Patch
+     * 
+     * @return array
+     */
+    private function updateRules(): array
+    {
+        $updated_id = $this->route('country');
+
+        return [
+            'country.name' => "string|unique:{$this->table},name,{$updated_id},id,deleted_at,NULL",
+            'country.alfa2' => "string|size:2|unique:{$this->table},alfa2,{$updated_id},id,deleted_at,NULL",
+            'country.alfa3' => "string|size:3|unique:{$this->table},alfa3,{$updated_id},id,deleted_at,NULL"
         ];
     }
 }
