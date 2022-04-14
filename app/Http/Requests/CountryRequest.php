68157095ec13dbb4f0440276fcfe8b395894555d
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-14 07:13:45 -0300

    adicionar retorno dos métodos

--- app/Http/Requests/CountryRequest.php
+++ app/Http/Requests/CountryRequest.php
@@ -11,7 +11,7 @@ class CountryRequest extends FormRequest
    *
    * @return bool
    */
-  public function authorize()
+  public function authorize(): bool
   {
     return true;
   }
@@ -21,7 +21,7 @@ class CountryRequest extends FormRequest
    *
    * @return array
    */
-  public function rules()
+  public function rules(): array
   {
     return [
       'country.name' => 'required|string|unique:countries,name',
