991acec5dbe4616286a61ee2900a0c41802d67b9
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-15 16:31:35 -0300

    adicionar tradução para request vazio

--- lang/pt-br/general.php
+++ lang/pt-br/general.php
@@ -8,12 +8,13 @@ return [
     |--------------------------------------------------------------------------
     |
     | The following language lines are used in general for various
-    | messages that we need to display to the user. You are free to modify
+    | messages that we need to display to the general. You are free to modify
     | these language lines according to your application's requirements.
     |
     */
 
   'unknown_error' => 'Ocorreu um erro desconhecido, por favor entre em contato com o desenvolvedor.',
+  'no_data' => 'Nenhum dado foi recebido, se de fato foi realizada alguma alteração por favor entre em contato com o suporte.',
 
 
 ];
