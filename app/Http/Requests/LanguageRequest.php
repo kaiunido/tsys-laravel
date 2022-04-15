83dc035b34791ff77d0b56103bd774a559280d9a
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-15 14:25:44 -0300

    criar regras para o form request de idioma

--- app/Http/Requests/LanguageRequest.php
+++ app/Http/Requests/LanguageRequest.php
@@ -7,13 +7,20 @@ use Illuminate\Foundation\Http\FormRequest;
 class LanguageRequest extends FormRequest
 {
     /**
+     * Model responsavel pelos requests.
+     * 
+     * @var $table
+     */
+    private $table = 'App\Models\Language';
+
+    /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +31,10 @@ class LanguageRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'language.name' => "required|string|filled|unique:{$this->table},name",
+            'language.code' => "required|string|filled|max:5|unique:{$this->table},code",
+            'language.locale' => 'nullable|string|filled|max:5',
+            'language.image' => 'nullable|string|filled',
         ];
     }
 }
