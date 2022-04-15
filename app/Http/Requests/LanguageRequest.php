6330e848917fe3cbbbc0e330ce7a62ca99271ff6
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-15 16:35:46 -0300

    separar as regras de update e store

--- app/Http/Requests/LanguageRequest.php
+++ app/Http/Requests/LanguageRequest.php
@@ -14,25 +14,50 @@ class LanguageRequest extends FormRequest
     private $table = 'App\Models\Language';
 
     /**
-     * Determine if the user is authorized to make this request.
+     * Determina se o usuário está autorizado para fazer a request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Pega as regras de validação para aplica ao request.
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
+    {
+        return $this->method() === 'PATCH' ? $this->updateRules() : $this->storeRules();
+    }
+
+    /**
+     * Retorna as regras do store
+     * 
+     * @return array
+     */
+    private function storeRules(): array
     {
         return [
-            'language.name' => "required|string|filled|unique:{$this->table},name",
-            'language.code' => "required|string|filled|max:5|unique:{$this->table},code",
+            'language.name' => "required|string|filled|unique:{$this->table},name,NULL,NULL,deleted_at,NULL",
+            'language.code' => "required|string|filled|max:5|unique:{$this->table},code,NULL,NULL,deleted_at,NULL",
+            'language.locale' => 'nullable|string|filled|max:5',
+            'language.image' => 'nullable|string|filled',
+        ];
+    }
+
+    /**
+     * Retorna as regras do update
+     * 
+     * @return array
+     */
+    private function updateRules(): array
+    {
+        return [
+            'language.name' => "nullable|string|filled|unique:{$this->table},name,deleted_at,NULL",
+            'language.code' => "nullable|string|filled|max:5|unique:{$this->table},code,deleted_at,NULL",
             'language.locale' => 'nullable|string|filled|max:5',
             'language.image' => 'nullable|string|filled',
         ];
