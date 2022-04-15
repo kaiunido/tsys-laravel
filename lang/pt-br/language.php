b812e646f84ad93427de1840967fb18593292276
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-15 14:27:44 -0300

    criar arquivo de i18n para idiomas

---
+++ lang/pt-br/language.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during language methods for various
+    | messages that we need to display to the language. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'updated' => 'O idioma de ID :id foi atualizado.',
+    'deleted' => 'O idioma de ID :id foi removido.',
+    'not_found' => 'O idioma de ID :id não foi encontrado.',
+];
