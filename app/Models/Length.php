d83c09c336693ff97626c3cea77a41c4dad237fc
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-18 16:53:47 -0300

    refatorar e adicionar retornos + prettify

--- app/Models/Length.php
+++ app/Models/Length.php
@@ -3,27 +3,43 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Length extends Model
+class Length extends BaseModel
 {
-  use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes;
 
-  /**
-   * Atributos que são atribuíveis em massa.
-   *
-   * @var array<int, string>
-   */
-  protected $fillable = [
-    'value'
-  ];
+    /**
+     * Atributos atribuíveis em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'value'
+    ];
 
-  /**
-   * Relacionamento com a descrição da unidade de medida
-   */
-  public function descriptions()
-  {
-    return $this->hasMany(LengthDescription::class);
-  }
+    /**
+     * Relacionamento com a descrição da unidade de medida.
+     *
+     * @return HasMany
+     */
+    public function descriptions(): HasMany
+    {
+        return $this->hasMany(LengthDescription::class);
+    }
+
+    /**
+     * Override para utilizar o evento "deleting" e deletar as descrições.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($length) {
+            $length->descriptions()->delete();
+        });
+    }
 }
