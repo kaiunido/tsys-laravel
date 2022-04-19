2a93885ebb25280ccdb7ac53eb093689d45474cc
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-19 20:12:14 -0300

    corrigir retornos + prettify

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -3,29 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Manufacturer extends Model
+class Manufacturer extends BaseModel
 {
-  use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes;
 
-  /**
-   * Atributos que são atribuíveis em massa.
-   *
-   * @var array<int, string>
-   */
-  protected $fillable = [
-    'name',
-    'image',
-    'sort_order',
-  ];
+    /**
+     * Atributos atribuíveis em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'image',
+        'sort_order',
+    ];
 
-  /**
-   * Relacionamento com produtos
-   */
-  public function products()
-  {
-    return $this->hasMany(Product::class);
-  }
+    /**
+     * Relacionamento com produtos
+     *
+     * @return HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
