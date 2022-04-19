b0451c4dcb6cd84259db754698e4b80b50ac0aa2
Carlos E. Evangelista <kaiunido@gmail.com>
2022-04-19 20:11:46 -0300

    prettify

--- database/migrations/2022_03_09_220219_create_manufacturers_table.php
+++ database/migrations/2022_03_09_220219_create_manufacturers_table.php
@@ -4,32 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('manufacturers', function (Blueprint $table) {
-      $table->id();
-      $table->string('name');
-      $table->string('image')->nullable();
-      $table->integer('sort_order')->default(0);
-      $table->timestamps();
-      $table->softDeletes();
-    });
-  }
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::dropIfExists('manufacturers');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
 };
